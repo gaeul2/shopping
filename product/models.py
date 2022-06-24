e11c9560408b0695f80e192701fe737b3c6d8c9d
gaeul <e0540@hanmail.net>
2022-06-24 15:41:04 +0900

    Add product option/review/like/cart/order_list 모델
    모델링 완료

--- product/models.py
+++ product/models.py
@@ -29,3 +29,37 @@ class Product(models.Model): #커피캡슐
         return self.name
 
 
+class ProductOption(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField("옵션이름", max_length=50)
+    content = models.TextField("옵션내용")
+    price = models.IntegerField("가격")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    product_option = models.ForeignKey(ProductOption, on_delete=models.CASCADE)
+    count = models.IntegerField("갯수")
+    is_paid = models.BooleanField(default=1) # 결제하면 0으로 바꾸자
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}의 장바구니'
+
+class OrderList(models.Model):
+    buyer = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    product_option = models.ForeignKey(ProductOption, on_delete=models.CASCADE)
+    pay_type = models.CharField("결제방식", max_length=80)
+    total_price = models.IntegerField("총금액")
+    state = models.CharField("주문상태", max_length=90) #결제완료,배송중,배송완료
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user}의 구매내역'
--- user/models.py
+++ user/models.py
@@ -42,3 +42,28 @@ class User(AbstractBaseUser):
     def is_staff(self):
         return self.is_admin
 
+
+class Review(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
+    rate = models.IntegerField("평점")
+    content = models.TextField("리뷰 내용")
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return f'{self.author}가 작성한 {self.product}에 대한 리뷰'
+
+
+class like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #
+    product = models.ForeignKey('product.Product', on_delete=models.CASCADE)
+
+    #중복 좋아요 방지 위한 user와 product에 제한걸기
+    #user/product는 pk값이 들어오므로 같은 pk로 같은 product_pk에 좋아요못함
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["user","product"],
+                                    name='unique_like')
+        ]
+
