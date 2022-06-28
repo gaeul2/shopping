cb307172327972d9735cac10d908d5518434de5e
gaeul <e0540@hanmail.net>
2022-06-28 23:18:21 +0900

    add 회원가입 기능

---
+++ user/serializers.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
+from user.models import User as UserModel
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ["username", "password", "fullname", "phone", "email", "is_active", "is_admin", "is_seller"]
+        extra_kwargs = {
+            'password' : {'write_only':True},
+        }
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        password = make_password(password)
+        user = UserModel(**validated_data)
+        user.password = password
+        user.save()
+        return user
--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from user.serializers import SignupSerializer
+from rest_framework import status
+from django.contrib.auth import authenticate
 
 # Create your views here.
+#todo 일반사용자/판매자 분류하기
+class UserSignupView(APIView):
+    def post(self, request):
+        signup_serializer = SignupSerializer(data=request.data)
+        if signup_serializer.is_valid():
+            signup_serializer.save()
+            return Response(signup_serializer.data, status=status.HTTP_200_OK)
+        return Response(signup_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Login(APIView):
+    def post(self, request):
+        user = authenticate(username=request.data['username'], password=request.data['password'])
