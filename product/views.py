9a5f81cf1d833e6808e7415c2e007f330613cfa4
gaeul <e0540@hanmail.net>
2022-06-29 12:52:42 +0900

    init 상품등록

--- product/views.py
+++ product/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class ProductView(APIView):
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+    def update(self,request):
+        pass
+
+    def delete(self,request):
+        pass
