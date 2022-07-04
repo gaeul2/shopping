5d697270ef2bab9fb4eff177e58c125ff6f00c11
gaeul <e0540@hanmail.net>
2022-07-04 15:16:53 +0900

    delete 카테고리 모델.
    커피머신이 카테고리 역할을 하여 삭제

--- product/models.py
+++ product/models.py
@@ -2,12 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField("카테고리명",max_length=50)
-
-    def __str__(self):
-        return self.name
-
 class CoffeeMachine(models.Model):
     brand = models.CharField("커피머신 브랜드", max_length=100)
 
@@ -16,7 +10,6 @@ class CoffeeMachine(models.Model):
 
 class Product(models.Model): #커피캡슐
     seller = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    category = models.ManyToManyField(Category)
     machine = models.ForeignKey(CoffeeMachine, on_delete=models.CASCADE)
     name = models.CharField("캡슐이름", max_length=100)
     explain = models.TextField("캡슐설명")
