3bdf9d7c6d1933f5cc5c1922b537937aff562a92
gaeul <e0540@hanmail.net>
2022-07-05 10:59:31 +0900

    ADD 리뷰기능
    리뷰 등록/조회/수정/삭제 가능

--- product/urls.py
+++ product/urls.py
@@ -3,8 +3,14 @@ from product import views
 
 
 urlpatterns =[
-    path('product/add/<int:id>', views.ProductView.as_view(), name='add_product'),
-    path('product/update/<int:id>', views.ProductView.as_view(), name='edit_product'),
-    path('product/delete/<int:id>', views.ProductView.as_view(), name='delete_product'),
-    path('product/<int:id>', views.ProductShowView.as_view(), name='products_by_machine'),
+    # 카테고리별 상품 등록/수정/제거/조회
+    path('product/add/<int:category_id>', views.ProductView.as_view(), name='add_product'),
+    path('product/update/<int:product_id>', views.ProductView.as_view(), name='edit_product'),
+    path('product/delete/<int:product_id>', views.ProductView.as_view(), name='delete_product'),
+    path('product/<int:category_id>', views.ProductShowView.as_view(), name='products_by_machine'),
+    
+    #상품 상세페이지 + 리뷰조회, 리뷰 등록/제거/수정
+    # path('product/<int:machine_id>/<int:product_id>', views..as_view(), name='product_detail'),
+    path('product/review/<int:product_id>', views.ReviewView.as_view(), name='create_review'),
+    path('product/review/<int:product_id>/<int:review_id>', views.ReviewView.as_view(), name='edit_review'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,10 +1,12 @@
+from functools import partial
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from product.serializers import ProductSerializer, ProductInfoSerializer
+from product.serializers import ProductSerializer, ProductInfoSerializer,ReviewSerializer
 from rest_framework import permissions
 from product.models import Product as ProductModel
 from product.models import CoffeeMachine as CoffeeMachineModel
+from user.models import Review as ReviewModel
 # Create your views here.
 
 class Selling_can_seller_and_Admin(permissions.BasePermission):
@@ -17,10 +19,10 @@ class Selling_can_seller_and_Admin(permissions.BasePermission):
 class ProductView(APIView):
     permission_classes = [Selling_can_seller_and_Admin]
 
-    def post(self,request,id): #카테고리 별 상품 등록위해 카테고리 id를 받음
+    def post(self,request, category_id): #카테고리 별 상품 등록위해 카테고리 id를 받음
         #1 -> 일리 2-> 네스프레소 3-> 네스카페 돌체구스토 4 ->라바짜
         request.data['seller'] = request.user.id
-        request.data['machine'] = id
+        request.data['machine'] = category_id
 
         product_serializer = ProductSerializer(data= request.data)
         if product_serializer.is_valid():
@@ -29,8 +31,8 @@ class ProductView(APIView):
         return Response(product_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-    def put(self,request,id):
-        product = ProductModel.objects.get(id=id)
+    def put(self,request,product_id):
+        product = ProductModel.objects.get(id=product_id)
         print(product)
         if request.user == product.seller:
             edit_serializer = ProductSerializer(product, data=request.data, partial=True)
@@ -40,9 +42,9 @@ class ProductView(APIView):
             return Response(edit_serializer.errors, status = status.HTTP_400_BAD_REQUEST)
         return Response(f'잘못된 접근입니다.')
 
-    def delete(self,request,id):
+    def delete(self,request,product_id):
         try:
-            product=ProductModel.objects.get(id=id)
+            product=ProductModel.objects.get(id=product_id)
             if request.user == product.seller or request.user.is_admin:
                 product.delete()
                 product.save()
@@ -53,7 +55,66 @@ class ProductView(APIView):
 
 #Done caffeemachine 별 조회
 class ProductShowView(APIView):
-    def get(self, request, id):
-        machine = CoffeeMachineModel.objects.get(id=id)
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, category_id):
+        machine = CoffeeMachineModel.objects.get(id=category_id)
         products_serializer = ProductInfoSerializer(ProductModel.objects.filter(machine=machine), many=True)
-        return Response(products_serializer.data, status = status.HTTP_200_OK)
+        return Response(products_serializer.data, status= status.HTTP_200_OK)
+
+
+#리뷰달기
+#1 상세페이지조회 젤 마지막 (완)
+#2. 리뷰 등록하자 (완)
+#3. 수정 삭제(완)
+#4. 권한 부여
+class ReviewView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+
+    def get(self, request, product_id):
+        reviews = ReviewModel.objects.filter(product_id=product_id)
+        review_serializer = ReviewSerializer(reviews, many=True)
+        return Response(review_serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, product_id):
+        try:
+            product = ProductModel.objects.get(id=product_id)
+        except ProductModel.DoesNotExist:
+            return Response(f'잘못된 접근입니다.')
+        else:
+            request.data['author'] = request.user.id
+            request.data['product'] = product.id
+            review_serializer = ReviewSerializer(data=request.data)
+            if review_serializer.is_valid():
+                review_serializer.save()
+                return Response(f'{product.name}에 대한 {request.user.fullname} 님의 리뷰가 등록되었습니다.', status=status.HTTP_200_OK)
+            return Response(review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def put(self, request, product_id, review_id):
+        product = ProductModel.objects.get(id=product_id)
+        review = ReviewModel.objects.get(id=review_id)
+        if request.user.id == review.author.id:
+            review_serializer = ReviewSerializer(review, data=request.data, partial=True)
+            if review_serializer.is_valid():
+                review_serializer.save()
+                return Response({"msg": f'리뷰수정이 완료되었습니다.'}, status= status.HTTP_200_OK )
+            return Response(review_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"msg": f'리뷰 작성자만 리뷰수정이 가능합니다.'}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def delete(self, request,product_id, review_id):
+        try :
+            product = ProductModel.objects.get(id=product_id)
+            review = ReviewModel.objects.get(id=review_id)
+        except ProductModel.DoesNotExist:
+            return Response({"msg" : f'잘못된 요청입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        except ReviewModel.DoesNotExist:
+            return Response({"msg" : f'잘못된 요청입니다.'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            if request.user.id == review.author.id:
+                review.delete()
+                review.save()
+                return Response({"msg":f'리뷰삭제가 완료되었습니다.'}, status=status.HTTP_200_OK)
+            return Response({"msg" : f'잘못된 요청입니다.'}, status=status.HTTP_400_BAD_REQUEST)
